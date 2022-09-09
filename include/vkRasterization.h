6fe0c7d160ed04ae3a67eff53c25f8ae7f7f980b
Elliott Ayling <elliottayling531@gmail.com>
2022-09-09 10:55:42 +0100

    Set up build

---
+++ include/vkRasterization.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <iostream>
+#define API_VULKAN
+#include "egaf.h"
+void test()
+{
+	
+}
---
+++ src/vkRasterization.cpp
@@ -0,0 +1,12 @@
+﻿// vkRasterization.cpp : Defines the entry point for the application.
+//
+
+#include "vkRasterization.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
